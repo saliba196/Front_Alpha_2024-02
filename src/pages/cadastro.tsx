5e938881702411c0504ce75f8bd8221438c4de69
Itravain <icarotravain@gmail.com>
2024-11-24 20:11:43 -0300

    Alterações finais cadastro

--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -15,34 +15,29 @@ const containerStyle = {
 };
 
 const paperStyle = {
-  padding: "50px 100px",
+  padding: "40px",
   textAlign: "center",
   borderRadius: "30px",
   backgroundColor: "#E1E3AC",
-  width: "400px",
+  width: "100%",
+  maxWidth: "500px",
 };
 
 const textFieldStyle = {
   "& .MuiOutlinedInput-root": {
     "& fieldset": {
-      borderColor: "#46685B",  // Cor da borda
+      borderColor: "#46685B",
       borderRadius: "20px",
       borderWidth: "4px",
       backgroundColor: "transparent",
     },
     "&.Mui-focused fieldset": {
-      borderColor: "#46685B", // Cor da borda ao focar
+      borderColor: "#46685B",
     },
     "& input": {
-      color: "#000",  // Cor do texto dentro do campo (garante visibilidade)
-      backgroundColor: "#D9D9D9", // Cor de fundo do campo de entrada
+      color: "#000",
+      backgroundColor: "#D9D9D9",
       borderRadius: "20px",
-    },
-    "& .MuiInputLabel-root": {
-      color: "#46685B",  // Cor do rótulo (label)
-    },
-    "& .MuiInputLabel-root.Mui-focused": {
-      color: "#46685B",  // Cor do rótulo ao focar
     },
   },
 };
@@ -50,8 +45,8 @@ const textFieldStyle = {
 const buttonStyle = {
   backgroundColor: "#648A64",
   borderRadius: "13px",
-  padding: "10px 20px", // Aumenta o espaçamento interno
-  fontSize: "1rem",   // Aumenta o tamanho da fonte
+  padding: "10px 20px",
+  fontSize: "1rem",
 };
 
 export const Cadastro = () => {
@@ -59,32 +54,20 @@ export const Cadastro = () => {
 
   return (
     <Container sx={containerStyle}>
-      <Stack spacing={3} sx={{ width: '100%', maxWidth: '450px' }}>
+      <Stack spacing={2} alignItems="center" sx={{ width: "100%", maxWidth: "500px" }}>
         <Paper sx={paperStyle}>
-          <Typography variant="h4" sx={{ marginBottom: '40px' }}>Cadastro</Typography>
+          <Typography variant="h3" sx={{ marginBottom: "30px" }}>
+            Cadastro
+          </Typography>
 
-          {/* Grid com campos lado a lado */}
           <Grid2 container spacing={2}>
-            {/* Nome Completo e Sobrenome */}
-            <Grid2 size ={{xs: 12, sm: 6}}>
-              <TextField
-                fullWidth
-                label="Nome Completo"
-                variant="outlined"
-                sx={textFieldStyle}
-              />
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <TextField fullWidth label="Nome Completo" variant="outlined" sx={textFieldStyle} />
             </Grid2>
-
-            {/* E-mail e Data de Nascimento */}
-            <Grid2 size ={{xs: 12, sm: 6}}>
-              <TextField
-                fullWidth
-                label="E-mail"
-                variant="outlined"
-                sx={textFieldStyle}
-              />
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <TextField fullWidth label="E-mail" variant="outlined" sx={textFieldStyle} />
             </Grid2>
-            <Grid2 size ={{xs: 12, sm: 6}}>
+            <Grid2 size={{ xs: 12, sm: 6 }}>
               <TextField
                 fullWidth
                 label="Data de Nascimento"
@@ -92,64 +75,57 @@ export const Cadastro = () => {
                 type="date"
                 sx={textFieldStyle}
                 InputLabelProps={{
-                  shrink: true,  // Necessário para o formato de data
+                  shrink: true,
                 }}
               />
             </Grid2>
-
-            {/* CPF */}
-            <Grid2 size ={{xs: 12, sm: 6}}>
-              <TextField
-                fullWidth
-                label="CPF"
-                variant="outlined"
-                sx={textFieldStyle}
-              />
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <TextField fullWidth label="CPF" variant="outlined" sx={textFieldStyle} />
             </Grid2>
-
-            {/* Senha e Confirmar Senha */}
-            <Grid2 size ={{xs: 12, sm: 6}}>
-              <TextField
-                fullWidth
-                label="Senha"
-                variant="outlined"
-                type="password"
-                sx={textFieldStyle}
-              />
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <TextField fullWidth label="Senha" variant="outlined" type="password" sx={textFieldStyle} />
             </Grid2>
-            <Grid2 size ={{xs: 12, sm: 6}}>
-              <TextField
-                fullWidth
-                label="Confirmar Senha"
-                variant="outlined"
-                type="password"
-                sx={textFieldStyle}
-              />
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <TextField fullWidth label="Confirmar Senha" variant="outlined" type="password" sx={textFieldStyle} />
             </Grid2>
           </Grid2>
 
-          {/* Já é cliente */}
           <FormControlLabel
             control={<Checkbox />}
-            label="Já é cliente?"
-            sx={{ marginBottom: '20px' }}
+            label="Já sou cliente Tina"
+            sx={{ justifyContent: "flex-begin", display: "flex", margin: "20px 0", color: "#46685B" }}
           />
 
-          {/* Botão de cadastro */}
           <Button
             variant="contained"
-            sx={{
-                buttonStyle,
-                alignSelf: "flex-end" // Alinha o botão à direita
-            }
-                
-            }
-            
-            onClick={() => navigate("/login")}
+            onClick={() => navigate("/home")}
+            fullWidth
+            sx={buttonStyle}
           >
             Cadastre-se
           </Button>
         </Paper>
+
+        {/* Texto abaixo do Paper */}
+        <Stack direction="row" spacing={1} alignItems="center" justifyContent="center" sx={{ marginTop: "20px" }}>
+          <Typography variant="body2" sx={{ color: "#D9D9D9" }}>
+            Já possui uma conta?
+          </Typography>
+          <Button
+            variant="text"
+            sx={{
+              textTransform: "none",
+              fontSize: "0.9rem",
+              color: "#D9D9D9",
+              "&:hover": {
+                color: "#213435",
+              },
+            }}
+            onClick={() => navigate("/")}
+          >
+            Faça login
+          </Button>
+        </Stack>
       </Stack>
     </Container>
   );
