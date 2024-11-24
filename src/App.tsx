aed9e80f42757fb3ba5e11e4281b12a4a97a8185
saliba196 <luiza.saliba196@gmail.com>
2024-11-24 11:16:57 -0300

    Componente de video semi ok.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
-import { MuiButton } from "./components/MuiButton";
+//import { MuiButton } from "./components/MuiButton";
 import ImgMediaCard from "C:/Users/luiza/Documents/teste/react-mui-demo/src/components/video_prev.tsx";
+import MediaControlCard from "C:/Users/luiza/Documents/teste/react-mui-demo/src/components/video_red.tsx";
+
 function App() {
   return (
     <div className="App">
-      <MuiButton />
+      {/*<MuiButton />*/}
       <ImgMediaCard />
+      <MediaControlCard />
     </div>
   );
 }
