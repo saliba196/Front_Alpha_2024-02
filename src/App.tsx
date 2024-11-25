161c3cbb5d0351bc28f1c66f7811eaa93f0077b2
andrecaly64 <105026467+andrecaly64@users.noreply.github.com>
2024-11-24 23:14:37 -0300

    inicio tela de infos dos cursos teste

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { MuiButtons } from "./components/MuiButtons";
 import { Login } from "./pages/login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Cadastro } from "./pages/cadastro";
+import { Infos } from "./pages/infos";
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/" element={<Infos/>} />
       </Routes>
     </Router>
   );
