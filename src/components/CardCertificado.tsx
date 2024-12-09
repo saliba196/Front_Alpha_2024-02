81e46fae08486fdb59a22dca99c902fe9885afdf
saliba196 <luiza.saliba196@gmail.com>
2024-12-09 16:14:08 -0300

    Botão de Download e card de certificado

--- src/components/CardCertificado.tsx
+++ src/components/CardCertificado.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Divider, Stack, Typography } from "@mui/material";
+import { Download } from "./Download";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
 
 interface VarsCardCertificado {
   imageUrl: string; // URL da imagem
@@ -16,21 +18,46 @@ export const CardCertificado: React.FC<VarsCardCertificado> = ({
 }) => {
   return (
     <Stack direction="column" spacing={1} alignItems="left">
-      <Stack direction="row" spacing={1} alignItems="left">
-        <Box
-          component="img"
-          src={imageUrl}
-          alt={altText}
-          sx={{
-            width: "190px",
-            height: "40px",
-            objectFit: "contain",
-          }}
-        />
-        <Stack direction="column" spacing={1} alignItems="left">
-          <Typography>{titulo}</Typography>
-          <Typography>{descricao}</Typography>
+      <Stack direction="row" justifyContent="space-between">
+        <Stack direction="row" spacing={1} alignItems="left">
+          <Box
+            component="img"
+            src={imageUrl}
+            alt={altText}
+            sx={{
+              width: "190px",
+              height: "40px",
+              objectFit: "contain",
+            }}
+          />
+          <Stack direction="column" spacing={1} alignItems="left">
+            <Typography
+              sx={{
+                fontFamily: "Nunito",
+                fontSize: "16px",
+                fontWeight: 700,
+                color: "white",
+                textAlign: "left",
+                marginBottom: "16px",
+              }}
+            >
+              {titulo}
+            </Typography>
+            <Typography
+              sx={{
+                fontFamily: "Nunito",
+                fontSize: "12px",
+                fontWeight: 400,
+                color: "white",
+                textAlign: "left",
+                marginBottom: "16px",
+              }}
+            >
+              {descricao}
+            </Typography>
+          </Stack>
         </Stack>
+        <Download icon={<FileDownloadIcon />} text="Donwload"></Download>
       </Stack>
       <Divider />
     </Stack>
