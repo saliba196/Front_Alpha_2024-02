35aab2e8872d73f18f7de917746a8a28dabb66e9
saliba196 <luiza.saliba196@gmail.com>
2024-12-09 11:31:18 -0300

    Nunito importado

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@fontsource/nunito/400.css"; // Nunito regular
+import "@fontsource/nunito/700.css"; // Nunito regular
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
